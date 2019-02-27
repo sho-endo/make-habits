498c8eda1a595b1fa7dae7cf3f610e3b279e9256
sho-endo <s.endo.8951@gmail.com>
2019-02-28 02:40:44 +0900

    Basic login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ユーザー登録しました"
-      redirect_to root_url
+      redirect_to @user
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
-  resources :users, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create, :show]
 end
