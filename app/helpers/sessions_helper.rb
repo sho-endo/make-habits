3b95e08a568daf0167d78cac8e06e47148f4ede9
sho-endo <s.endo.8951@gmail.com>
2019-03-04 02:24:43 +0900

    Implement login requirement and friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,4 +36,14 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # for friendly forwarding
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
