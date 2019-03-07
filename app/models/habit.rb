c8c30b19a7dbbc6325188fd0c775650cb17589d4
sho-endo <s.endo.8951@gmail.com>
2019-03-08 02:29:18 +0900

    Add validation

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,5 +1,7 @@
 class Habit < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
+  validates :rule1, presence: true, length: { maximum: 255 }
+  validates :rule2, presence: true, length: { maximum: 255 }
   validates :type, presence: true
   validates :user_id, presence: true
 
