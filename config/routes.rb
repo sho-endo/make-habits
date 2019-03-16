a7604186c38971a6e2100a95ed5c3cece5916df7
sho-endo <s.endo.8951@gmail.com>
2019-03-16 09:16:32 +0900

    Implement habit destroy function

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
+  resources :habits, only: [:destroy]
   get "/habits/select", to: "habits#select"
 
   resources :makes, only: [:create, :show]
